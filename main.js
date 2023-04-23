d4ec8902e865b49e2cf2e0ab5152a22a004f4deb
Pinkmuli <kimjh0995@gmail.com>
2023-04-23 18:07:53 -0400

    added pagination

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let newsArray = [];
+let page = 1;
+let totalPages = 0;
 let menu = document.querySelectorAll(".menu button");
 menu.forEach((menu) =>
 	menu.addEventListener("click", (event) => getNewsByTopic(event))
@@ -8,20 +11,24 @@ const getNews = async () => {
 		let header = new Headers({
 			"x-api-key": "84KiV3HrOWlXN5HbVYuPOWt_LRDMj2iYfxikTDr_PXg",
 		});
+		url.searchParams.set("page", page); //setting the page query
 		let response = await fetch(url, { headers: header });
 		let data = await response.json();
+		console.log(response.status);
 		if (response.status == 200) {
 			if (data.total_hits == 0) {
 				throw new Error("No search Result");
 			}
+			console.log("data received", data);
 			newsArray = data.articles; //array
-			console.log(newsArray);
+			totalPages = data.total_pages;
+			page = data.page;
 			render();
+			pagination();
 		} else {
 			throw new Error(data.message);
 		}
 	} catch (error) {
-		console.log(error.message);
 		alert(error.message, "danger");
 	}
 };
@@ -42,7 +49,7 @@ function alert(message, type) {
 
 const getLatestNews = async () => {
 	url = new URL(
-		"https://api.newscatcherapi.com/v2/latest_headlines?countries=CA&topic=business"
+		"https://api.newscatcherapi.com/v2/latest_headlines?countries=CA&topic=business&page_size=10"
 	);
 	console.log(url);
 	getNews();
@@ -68,7 +75,7 @@ const getNewsByKeyword = async () => {
 };
 
 const render = () => {
-	let newsHTML = "";
+	let newsHTML = ``;
 	//don't use for loop
 	newsHTML = newsArray
 		.map((item) => {
@@ -90,7 +97,75 @@ const render = () => {
 		.join("");
 
 	document.getElementById("news-board").innerHTML = newsHTML;
-	console.log(newsHTML);
+};
+
+const pagination = () => {
+	// 1.1~5까지를 보여준다
+	// 2.6~10을 보여준다 => last, first 가필요
+	// 3.만약에 first가 6 이상이면 prev 버튼을 단다
+	// 4.만약에 last가 마지막이 아니라면 next버튼을 단다
+	// 5.마지막이 5개이하이면 last=totalpage이다
+	// 6.페이지가 5개 이하라면 first = 1이다
+	let paginationHTML = ``;
+	let pageGroup = Math.ceil(page / 5);
+
+	let lastPage = pageGroup * 5;
+	if (lastPage > totalPages) {
+		lastPage = totalPages;
+	}
+	//if last pageGroup has less than 5 pages, set lastPage=totalPages, otherwise lastPage = pageGroup*5
+	let firstPage = lastPage - 4 <= 0 ? 1 : lastPage - 4;
+
+	//add previous button if firstPage of the pageGroup is 6 or higher
+	if (firstPage >= 6) {
+		paginationHTML += `
+		<li class="page-item">
+	  		<a class="page-link" href="#" aria-label="Previous" onclick="moveToPage(1)">
+	    	<span aria-hidden="true">&laquo;</span>
+	 	 	</a>
+		</li>
+		<li class="page-item">
+	  		<a class="page-link" href="#" aria-label="Previous" onclick="moveToPage(${
+					page - 1
+				})">
+	    	<span aria-hidden="true">&lt;</span>
+	 	 	</a>
+		</li>`;
+	}
+	for (let i = firstPage; i <= lastPage; i++) {
+		paginationHTML += `	
+		<li class="page-item">
+			<a class="page-link ${
+				page == i ? "active" : ""
+			} href="#" onclick="moveToPage(${i})">
+				${i}
+			</a>
+		</li>`;
+	}
+	if (lastPage < totalPages) {
+		paginationHTML += `
+		<li class="page-item">
+			<a class="page-link" href="#" aria-label="Next" onclick="moveToPage(${
+				page + 1
+			})">
+	    	<span aria-hidden="true"> &gt;</span>
+	  		</a>
+		</li>
+		<li class="page-item">
+			<a class="page-link" href="#" aria-label="Next" onclick="moveToPage(${totalPages})">
+	    	<span aria-hidden="true">&raquo;</span>
+	  		</a>
+		</li>`;
+	}
+
+	document.querySelector(".pagination").innerHTML = paginationHTML;
+};
+
+const moveToPage = (pageNum) => {
+	page = pageNum;
+	console.log(page);
+
+	getNews();
 };
 
 let searchButton = document.getElementById("search-button");
